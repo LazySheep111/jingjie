7fa35cb989297362fb971f4912a75b68a0402de6
itcast <25231@qq.com>
2026-09-20 18:25:01 +0800

    chore: remove legacy root source tree

--- src/main/java/com/novelgeneration/novel/service/GenerateOutlineService.java
+++
@@ -1,8 +0,0 @@
-package com.novelgeneration.novel.service;
-
-import com.novelgeneration.novel.dto.OutlineDTO;
-import com.novelgeneration.novel.vo.NovelOutlineVO;
-
-public interface GenerateOutlineService{
-    NovelOutlineVO generateOutline(OutlineDTO outlineDTO);
-}
